e6284e739406b9cb616b5bd70d08729f25c706f2
Nick B <nick_beeson@yahoo.com>
2016-03-16 12:54:53 -0600

    sum code for the cards

---
+++ CardGame/Assets/Scripts/Library.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Library : MonoBehaviour {
+    private ArrayList cardList;
+	// Use this for initialization
+	void Start () {
+        cardList.Add(12);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Card 
+{
+    private int index;
+    private string name;
+    private string sprite;
+    private int hitpoints;
+    private int attackpoints;
+   private float faceoff;
+    public Card(int i, string n, string s, int hp, int ap, float fo)
+    {
+        index = i;
+        name = n;
+        sprite = s;
+        hitpoints = hp;
+        attackpoints = ap;
+        faceoff = fo;
+    }
+ int getHp()
+    {
+        return hitpoints;
+    }
+    int getAp()
+    {
+        return attackpoints;
+    }
+    string getName()
+    {
+        return name;
+    }
+    float getFaceoff()
+    {
+        return faceoff;
+    }
+}
